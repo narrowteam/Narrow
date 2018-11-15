017a7967e95dc3e7eee34da88b79504cb50acc77
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2018-11-15 19:22:16 +0100

    fixreq

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '~vuetify/src/stylus/main'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
